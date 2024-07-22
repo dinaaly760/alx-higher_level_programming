3ff30689b1411fc3ee13767bea9bae48c3d5ae7c
Dina Aly <dinaaly760@gmail.com>
2024-07-21 21:56:33 -0700

    files

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,21 +1,3 @@
 #!/bin/bash
-
-# Check if a URL was provided as an argument
-if [ -z "$1" ]; then
-    echo "Usage: $0 <url>"
-    exit 1
-fi
-
-# Store the URL in a variable
-url="$1"
-
-# Send the GET request with the specified header using curl
-response=$(curl -i -H "X-School-User-Id: 98" "$url")
-
-# Display the response code and headers
-echo "Response code: $?"
-echo "Response headers:"
-echo "$response" | grep -E "^HTTP|^X-"
-
-# Display the response body
-echo "$response" | sed -n '/^\r$/,$p'
+# script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
+curl -s "$1" -H "X-School-User-Id: 98"
