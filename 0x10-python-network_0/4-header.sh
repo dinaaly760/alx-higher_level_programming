20953ce617a8deb868c7497322109a65535d9d6e
Dina Aly <dinaaly760@gmail.com>
2024-07-21 21:40:36 -0700

    0x10-python-network_0

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 # script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -s "$1" -H "X-School-User-Id: 98" | cat
+if [ -z "$1" ]; then
+  echo "Usage: $0 <URL>"
+  exit 1
+fi
+
+response=$(curl -s -w "%{http_code}" -o /dev/null "$1" -H "X-School-User-Id: 98")
+if [ "$response" -ne 200 ]; then
+  echo "Error: $response"
+else
+  curl -s "$1" -H "X-School-User-Id: 98" | cat
+fi
